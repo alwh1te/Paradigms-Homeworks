2a6331cd36d160f6b572130bfc60e9aa2adc188e
alw <alexander.s.filatov@gmail.com>
2024-03-14 14:57:47 +0300

    commiting hw06

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -1,10 +1,10 @@
 "use strict"
 const operation = operation => (...ops) => (...operands) => operation(...ops.map(op => op(...operands)))
 const cnst = x => () => Number(x);
+const varInd = ['x', 'y', 'z']
 const variable = name => (...args) => args[(varInd.indexOf(name))]
 const add = operation((a, b) => a + b)
 const subtract = operation((a, b) => a - b)
 const multiply = operation((a, b) => a * b)
 const divide = operation((a, b) => a / b)
 const negate = operation(a => -a)
-const varInd = ['x', 'y', 'z']
