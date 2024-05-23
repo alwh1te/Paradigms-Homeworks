9f9e70418e55ef9e86abec505b08db2934580b07
alw <alexander.s.filatov@gmail.com>
2024-05-23 17:13:21 +0300

    tree map done

--- prolog-solutions/tree-map.pl
+++ prolog-solutions/tree-map.pl
@@ -1,12 +1,3 @@
-abs(X, X) :- X >= 0.
-abs(X, Y) :- X < 0, Y is -X.
-
-length([], 0).
-length([_|Tail], N) :-
-  length(Tail, M),
-  N is M + 1.
-
-
 map_build([], nil).
 
 map_build(List, TreeMap) :-
@@ -17,7 +8,6 @@ map_build(List, TreeMap) :-
   map_build(Right, RightSubTree),
   TreeMap = tree(Key, Value, LeftSubTree, RightSubTree).
 
-
 split_at(N, List, [], List) :-
   N =< 0.
 
@@ -25,49 +15,6 @@ split_at(N, [X|Xs], [X|Ys], Zs) :-
   N > 0,
   M is N - 1,
   split_at(M, Xs, Ys, Zs).
-
-
-map_insert(nil, Key, Value, tree(Key, Value, nil, nil)).
-
-map_insert(tree(Key, Value, Left, Right), Key, Value, tree(Key, Value, Left, Right)).
-
-map_insert(tree(RootKey, RootValue, Left, Right), Key, Value, NewTree) :-
-  Key < RootKey,
-  map_insert(Left, Key, Value, SubTree),
-  update_height(tree(RootKey, RootValue, SubTree, Right), NewTree).
-
-map_insert(tree(RootKey, RootValue, Left, Right), Key, Value, NewTree) :-
-  Key > RootKey,
-  map_insert(Right, Key, Value, SubTree),
-  update_height(tree(RootKey, RootValue, Left, SubTree), NewTree).
-
-
-update_height(tree(Key, Value, Left, Right), tree(Key, Value, Left, Right)) :-
-  height(Left, LH),
-  height(Right, RH),
-  Diff is LH - RH,
-  abs(Diff) =< 1.
-
-update_height(tree(Key, Value, Left, Right), NewTree) :-
-  height(Left, LH),
-  height(Right, RH),
-  Diff is LH - RH,
-  abs(Diff) > 1,
-  rotate(tree(Key, Value, Left, Right), NewTree).
-
-rotate(tree(K1, V1, tree(K2, V2, L2, R2), R1), tree(K2, V2, L2, tree(K1, V1, R2, R1))) :-
-  height(L2, H1),
-  height(R2, H2),
-  H1 >= H2.
-
-rotate(tree(K1, V1, L1, tree(K2, V2, L2, R2)), tree(K2, V2, tree(K1, V1, L1, L2), R2)).
-
-height(nil, 0).
-
-height(tree(_, _, Left, Right), H) :-
-  height(Left, LH),
-  height(Right, RH),
-  H is max(LH, RH) + 1.
 
 
 map_get(tree(Key, Value, _, _), Key, Value).
@@ -79,3 +26,13 @@ map_get(tree(RootKey, _, Left, _), Key, Value) :-
 map_get(tree(RootKey, _, _, Right), Key, Value) :-
   Key > RootKey,
   map_get(Right, Key, Value).
+
+
+map_lastKey(tree(Key, Value, Left, nil), Key):- !.
+map_lastKey(tree(Key, Value, Left, Right), K) :- map_lastKey(Right, K).
+
+map_lastValue(tree(Key, Value, Left, nil), Value):- !.
+map_lastValue(tree(Key, Value, Left, Right), V) :- map_lastValue(Right, V).
+
+map_lastEntry(tree(Key, Value, Left, nil), (Key, Value)):- !.
+map_lastEntry(tree(Key, Value, Left, Right), (K, V)):- map_lastEntry(Right, (K, V)).
